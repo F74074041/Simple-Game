f4764af12ce553f9182c25d182fe7cc91f33308f
F74074041 <w0918909311@gmail.com>
2022-04-28 19:36:55 +0800

    first commit

---
+++ 6_2/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace _6_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(536, 116);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "重新開始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(551, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 21);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "score";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::_6_2.Properties.Resources.Invader;
+            this.pictureBox3.Location = new System.Drawing.Point(121, 30);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(55, 50);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.pictureBox2.Enabled = false;
+            this.pictureBox2.Location = new System.Drawing.Point(492, -8);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(40, 470);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::_6_2.Properties.Resources.ship_1;
+            this.pictureBox1.Location = new System.Drawing.Point(195, 395);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(53, 55);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1500;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(597, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 21);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "0";
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(654, 462);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer3;
+    }
+}
+
---
+++ 6_2/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _6_2
+{
+    public partial class Form1 : Form
+    {
+        int i=0;
+        int score = 0;
+        public  PictureBox[] pic = new PictureBox[1000];
+        int c = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            i = 0;
+            timer1.Enabled = true;
+            pictureBox3.Image = Image.FromFile("Invader.png");
+            pictureBox3.Hide();
+            pictureBox1.Image = Image.FromFile("ship 1.png");
+            timer1.Start();
+            timer3.Start();
+            
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    pictureBox1.Left -= 5;
+                    if (pictureBox1.Left < 0)
+                    {
+                        pictureBox1.Left += 20;
+                    }
+                    break;
+                case Keys.Right:
+                    pictureBox1.Left += 5;
+                    if (pictureBox1.Left+30 > pictureBox2.Left)
+                    {
+                        pictureBox1.Left -= 20;
+                    }       
+                    break;
+                case Keys.Up:
+                    pictureBox1.Top -= 5;
+                    if (pictureBox1.Top <= 0)
+                    {
+                        pictureBox1.Top += 20;
+                    }
+                    break;
+                case Keys.Down:
+                    pictureBox1.Top += 5;
+                    if (pictureBox1.Top >240)
+                    {
+                        pictureBox1.Top -= 20;
+                    }
+                    break;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+      
+        }
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            label2.Text = score.ToString();
+            Random r = new Random();
+            int x = r.Next(0, pictureBox2.Left-10);
+            pic[c] = new PictureBox();        
+            pic[c].Image = pictureBox3.Image;
+            pic[c].SizeMode = PictureBoxSizeMode.Zoom;
+            pic[c].Size = new Size(20, 20);
+            pic[c].Left = x;
+            score++;
+            timer2.Start();
+            this.Controls.Add(pic[c]);
+           // pic[c].Top += 10;
+            if (c < 999)
+            {
+                c++;
+            }
+            else
+            {
+                timer1.Stop();
+                c = 0;
+            }
+            
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            for (i = 0; i < c; i++)
+            {
+                pic[i].Top += 10;
+            }
+            
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            timer1.Start();
+            timer2.Start();
+            timer3.Start();
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            for (int j = 0; j < c; j++)
+            {
+
+                if ((pic[j].Top <= pictureBox1.Top + pictureBox1.Height && pic[j].Top >= pictureBox1.Top - pic[j].Height) && pic[j].Left <= pictureBox1.Left + pictureBox1.Width && pic[j].Left >= pictureBox1.Left - pic[j].Width)
+                {
+                    timer1.Stop();
+                    timer2.Stop();
+                    timer3.Stop();
+                    DialogResult a = MessageBox.Show("gameover");
+                    if (a == DialogResult.OK)
+                    {
+                        score = 0;
+                        pictureBox1.Location = new Point(130, 230);
+                        button1.Enabled = true;
+                        for (int i = 0; i < c; i++)
+                        {
+                            pic[i].Location = new Point(-100, -100);
+                            pic[i].Dispose();
+                        }
+                        label2.Text = score.ToString();
+                    }
+
+                }
+            }
+        }
+    }
+}
